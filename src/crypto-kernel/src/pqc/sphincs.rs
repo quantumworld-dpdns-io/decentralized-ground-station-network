e37fc4bdcb3dcb56a182562880b6344868a4e0fc
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-25 22:05:07 +0800

    Auto-commit: Project implementation in progress [count: 1281]

--- src/crypto-kernel/src/pqc/sphincs.rs
+++ src/crypto-kernel/src/pqc/sphincs.rs
@@ -5,13 +5,13 @@ use sha3::digest::{ExtendableOutput, Update};
 use sha3::{Sha3_256, Sha3_512, Shake256};
 use zeroize::Zeroize;
 
-const SPHINCS128F_PK_SIZE: usize = 32;
+const SPHINCS128F_PK_SIZE: usize = 33;
 const SPHINCS128F_SK_SIZE: usize = 65;
 const SPHINCS128F_SIG_SIZE: usize = 17088;
-const SPHINCS192S_PK_SIZE: usize = 48;
+const SPHINCS192S_PK_SIZE: usize = 49;
 const SPHINCS192S_SK_SIZE: usize = 97;
 const SPHINCS192S_SIG_SIZE: usize = 35664;
-const SPHINCS256F_PK_SIZE: usize = 64;
+const SPHINCS256F_PK_SIZE: usize = 65;
 const SPHINCS256F_SK_SIZE: usize = 129;
 const SPHINCS256F_SIG_SIZE: usize = 49856;
 
